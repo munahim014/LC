8aec7adfebe1bc1b6447ab3bf4303a51a0465354
munahim014 <100985288+munahim014@users.noreply.github.com>
2022-04-13 14:04:56 -0400

    Time: 6 ms (96.06%), Space: 54.8 MB (89.20%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -13,10 +13,19 @@ class Solution {
 //         return false;
 //     }
     //1123
+//      public boolean containsDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+//         for (int i = 0; i < nums.length-1; i++){
+//             if (nums[i] == nums[i+1]) return true;
+//         }
+        
+//         return false;
+//     }
      public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length-1; i++){
-            if (nums[i] == nums[i+1]) return true;
+        Set<Integer> number = new HashSet<Integer>();
+        for (int num : nums){
+            if (number.contains(num)) return true;
+            else number.add(num);
         }
         
         return false;
